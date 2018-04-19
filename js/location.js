8c320a88161ad24edaaa29a59871fc9e5b9dbd24
Vih Modeste <dvmodeste@yahoo.fr>
2018-04-18 23:48:08 -0500

    init

---
+++ js/location.js
@@ -0,0 +1,11 @@
+function initMap() {
+	var uluru = {lat:5.359952, lng: -4.008256};
+	var map = new google.maps.Map(document.getElementById('location'), {
+		zoom: 6,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
